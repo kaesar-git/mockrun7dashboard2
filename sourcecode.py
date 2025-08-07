83732a520ce745dac5a29ac239738d373b36d4f2
kaesar-git <kaesaralmajiid@gmail.com>
2025-08-07 18:35:15 +0700

    Update sourcecode.py

--- sourcecode.py
+++ sourcecode.py
@@ -218,8 +218,8 @@ with tabs[3]:
         }
 
         .stTabs [data-baseweb="tab"][aria-selected="true"] {
-            color: #113F67 !important;  /* warna teks aktif */
-            border-bottom: 3px solid #113F67 !important;  /* warna garis bawah aktif */
+            color: #053364 !important;  /* warna teks aktif */
+            border-bottom: 3px solid #053364 !important;  /* warna garis bawah aktif */
             font-weight: bold;
         }
     </style>
